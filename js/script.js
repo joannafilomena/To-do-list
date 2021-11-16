b6bcad3d4c81e33e6984fc2a83ebc47f8fba4594
Joanna Kamińska <joannakaminska-92@wp.pl>
2021-11-16 13:57:08 +0100

    Added remove button

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 let list = document.querySelector(".list");
 let addTaskButton = document.querySelector(".addTask");
 let inputTask = document.querySelector(".input");
-
+let removeTaskButtons = null;
+let li = null;
 products = [];
 
 addTaskButton.addEventListener("click", (event) => {
     event.preventDefault();
 
-    products.forEach((product) => {
-        list.innerHTML += "<li>" + product + "</li>";
-    });
     products.push(inputTask.value);
+    list.innerHTML = "";
 
+    products.forEach((product) => {
+
+        list.innerHTML += "<li>" + product + "<button class=\"bin\">Usuń</buton></li>";
+    });
+
+    removeTaskButtons = document.querySelectorAll(".bin");
+
+    removeTaskButtons.forEach((removeButton) => {
+        removeButton.addEventListener("click", (li) => {
+            li = document.querySelector("li");
+            li.remove();
+        });
+    });
 })
 
-/*let removeTaskButton = document.querySelector(".bin");
 
-    removeTaskButton.addEventListener("click", (event) => {
+/*removeTaskButtons.forEach((removeButton) => {
+    removeButton.addEventListener("click", (event) => {
         event.preventDefault();
-        products.forEach((product) => {
-            list.innerHTML = "";
-        });
-    })*/
+        products.splice(inputTask.value);
+    })
+})*/
+
